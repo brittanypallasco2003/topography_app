e25d8f362812b25e9008032fb90cfc5702c10e45
brittanypallasco2003 <nohemiespinel0@gmail.com>
2024-08-02 21:39:40 -0500

    listar usuarios y buscador

--- components/InfoUser.js
+++ components/InfoUser.js
@@ -1,17 +1,52 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React, { useContext } from "react";
-
+import { moderateScale, scale } from "react-native-size-matters";
+import { IconButton, List, useTheme } from "react-native-paper";
 
 const InfoUser = ({ item }) => {
+  const theme = useTheme();
+  const { email, role } = item;
 
   return (
-    <View
-      style={{ padding: 10, borderBottomWidth: 1, borderBottomColor: "#ccc" }}
-    >
-      <Text>Email: {item.email}</Text>
-      <Text>Role: {item.role}</Text>
-    </View>
+    <>
+      <List.Section>
+        <List.Item
+          title={`${email}`}
+          description={`${role}`}
+          style={{ borderBottomWidth: moderateScale(1) }}
+          titleStyle={style.titleStListItem}
+          descriptionStyle={style.descripListItem}
+          right={() => (
+            <>
+              <IconButton
+                icon="delete"
+                iconColor={theme.colors.error}
+                size={scale(18)}
+                onPress={() => {}}
+              />
+              {/* <IconButton  //te dejo esto por si quieres poner mejor una sola vista para eliminar y desactivar usuarios
+                icon="account-off"
+                iconColor={theme.colors.primary}
+                size={scale(18)}
+                onPress={() => {}}
+              /> */}
+            </>
+          )}
+        />
+      </List.Section>
+    </>
   );
 };
+
+const style = StyleSheet.create({
+  titleStListItem: {
+    fontFamily: "Poppins_600SemiBold",
+    fontSize: scale(12.5),
+  },
+  descripListItem: {
+    fontFamily: "Poppins_500Medium",
+    fontSize: scale(11),
+  },
+});
 
 export default InfoUser;
--- screens/ListaUser.js
+++ screens/ListaUser.js
@@ -1,22 +1,37 @@
 import { View, Text, FlatList } from "react-native";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import InfoUser from "../components/InfoUser";
 import { LocationContext } from "../context/LocationContext";
+import { Searchbar } from "react-native-paper";
+import { moderateScale, scale, verticalScale } from "react-native-size-matters";
 
 const ListaUser = () => {
   const { userInfo, setuserInfo } = useContext(LocationContext);
-  return (
-    <SafeAreaView style={{flex:1}}>
-      <FlatList
-      data={userInfo}
-      renderItem={({item}) => {
-        return <InfoUser item={item}/>
-       }}
-       keyExtractor={(item) => item.key}
-      />
+  const [searchQuery, setSearchQuery] = useState("");
+  const usuariosFiltrados = userInfo.filter((user) =>
+    user.email.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
-    </SafeAreaView>
+  return (
+    <>
+      <SafeAreaView style={{ flex: 1, paddingHorizontal: moderateScale(20) }}>
+        <Searchbar
+          placeholder="Usuario..."
+          inputStyle={{ fontFamily: "Poppins_500Medium", fontSize: scale(13) }}
+          onChangeText={setSearchQuery}
+          value={searchQuery}
+        />
+        <FlatList
+          data={usuariosFiltrados}
+          renderItem={({ item }) => {
+            return <InfoUser item={item} />;
+          }}
+          keyExtractor={(item) => item.key}
+          showsVerticalScrollIndicator={false}
+        />
+      </SafeAreaView>
+    </>
   );
 };
 
